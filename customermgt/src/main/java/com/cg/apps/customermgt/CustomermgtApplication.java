753e84a2f9eb3897d68cfcdced45fb5c90eb37b6
jatint17 <jatintakkar@ymail.com>
2021-03-11 20:51:46 +0530

    Sprint 1

---
+++ customermgt/src/main/java/com/cg/apps/customermgt/CustomermgtApplication.java
@@ -0,0 +1,13 @@
+package com.cg.apps.customermgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomermgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomermgtApplication.class, args);
+	}
+
+}
---
+++ customermgt/src/test/java/com/cg/apps/customermgt/CustomermgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.apps.customermgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomermgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
